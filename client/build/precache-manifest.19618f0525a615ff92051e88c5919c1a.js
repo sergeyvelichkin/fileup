933bbe2d10f96b350b4963f5763801a161fbdf74
Sergei Velichkin <sergey.velichkin@gmail.com>
2020-10-14 17:00:58 -0600

    depl

---
+++ client/build/precache-manifest.19618f0525a615ff92051e88c5919c1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa80000d11fac979f196",
+    "url": "/static/css/main.9275e55d.chunk.css"
+  },
+  {
+    "revision": "fa80000d11fac979f196",
+    "url": "/static/js/main.fa80000d.chunk.js"
+  },
+  {
+    "revision": "ae7fd5f708b58a6f46b3",
+    "url": "/static/js/1.ae7fd5f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "317816885c74c803b2b2b18441a9df91",
+    "url": "/index.html"
+  }
+];
